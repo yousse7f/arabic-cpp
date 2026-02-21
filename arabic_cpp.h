ebf882496bbe99318a9e781d6434a4da183d430e
yousse7f <awawa7780@gmail.com>
2026-02-22 01:56:36 +0300

    Initial commit with updated terminology (Line Break to Enzel)

---
+++ arabic_cpp.h
@@ -0,0 +1,37 @@
+#ifndef ARABIC_CPP_H
+#define ARABIC_CPP_H
+
+#include <iostream>
+
+// الكلمات المفتاحية الأساسية
+#define صحيح int
+#define عشري float
+#define مزدوج double
+#define حرف char
+#define فارغ void
+
+// الجمل الشرطية والدورات
+#define إذا if
+#define وإلا else
+#define لأجل for
+#define بينما while
+#define افعل do
+#define توقف break
+#define أرجع return
+
+// القيم المنطقية
+#define صح true
+#define خطأ false
+
+// المدخلات والمخرجات
+#define أخرج std::cout
+#define أدخل std::cin
+#define إنزل std::endl
+#define القياسي std
+#define الرئيسية main
+
+// ملاحظة: لا يمكن تغيير #include برمجياً بسهولة عبر macros
+// ولكن يمكن كتابة تعليق توضيحي للمستخدمين
+// #تضمين <اسم_المكتبة> -> #include <library_name>
+
+#endif // ARABIC_CPP_H
