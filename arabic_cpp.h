739a6466abf6b9d8ec35a792c5da0bfbbf9d887b
yousse7f <awawa7780@gmail.com>
2026-02-22 03:10:13 +0300

    Final Refinements: Enhanced UI animations and updated interactive code samples

--- arabic_cpp.h
+++ arabic_cpp.h
@@ -9,8 +9,11 @@
 #define مزدوج double
 #define حرف char
 #define فارغ void
+#define ثابت const
+#define طويل long
+#define قصير short
 
-// الجمل الشرطية والدورات
+// الجمل الشرطية والتحكم
 #define إذا if
 #define وإلا else
 #define دورة for
@@ -18,6 +21,9 @@
 #define افعل do
 #define توقف break
 #define أرجع return
+#define تبديل switch
+#define حالة case
+#define تلقائي default
 
 // القيم المنطقية
 #define صح true
@@ -29,6 +35,15 @@
 #define إنزل std::endl
 #define القياسي std
 #define الرئيسية main
+
+// البرمجة الكائنية والأسماء
+#define صنف class
+#define بنية struct
+#define عام public
+#define خاص private
+#define محمي protected
+#define استخدم_فضاء_الاسماء using namespace
+#define فضاء_الاسماء namespace
 
 // ملاحظة: لا يمكن تغيير #include برمجياً بسهولة عبر macros
 // ولكن يمكن كتابة تعليق توضيحي للمستخدمين
